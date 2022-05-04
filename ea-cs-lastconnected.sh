6ff8f509e476e0513c8501ffd7c9a01d1460bdf7
Richard Purves <franton@users.noreply.github.com>
2022-05-04 12:41:48 -0700

    Create ea-cs-lastconnected.sh

---
+++ ea-cs-lastconnected.sh
@@ -0,0 +1,19 @@
+#!/bin/zsh
+
+# Report the last Crowdstrike connection to the cloud
+# Original EA: https://github.com/zoocoup/CrowdstikeEAsforJamfPro/blob/main/CrowdstrikeFalconv6LastEstablishedEA.sh
+
+falconctl=$( /usr/bin/find /Applications -iname "falconctl" -type f -maxdepth 4 )
+
+if [ "$falconctl" ];
+then
+	test=$( "$falconctl" stats | /usr/bin/awk '/Cloud Activity | Last Established At/ {print $4,$5,$6,$8}' )
+    if [ ! -z "$test" ];
+    fi
+		echo "<result>$( /bin/date -j -f "%b %d, %Y %H:%M:%S" "$test" "+%Y-%m-%d %H:%M:%S" )</result>"
+    else
+    	echo "<result>1970-01-01 09:00:00</result>"
+    fi
+else
+	echo "<result>1970-01-01 09:00:00</result>"
+fi
