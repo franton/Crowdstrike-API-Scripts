26fa5601be271c93fd24a51e13e0949fed01a65e
Richard Purves <franton@users.noreply.github.com>
2022-05-09 09:18:56 -0700

    Update install-csf.sh
    check for existing installation

--- install-csf.sh
+++ install-csf.sh
@@ -3,60 +3,68 @@
 # Download latest specified Crowdstrike pkg and install
 # richard@richard-purves.com - 05/03/2022
 
-# Client ID, Client Secret for the API token. Then make a base64 version.
-clientid=""
-secret=""
-b64creds=$( printf "$clientid:$secret" | /usr/bin/iconv -t ISO-8859-1 | /usr/bin/base64 -i - )
+# Is Crowdstrike already installed
+if [ ! -d "/Applications/Falcon.app" ];
+then
+	echo "Crowdstrike missing. Proceeding with install."
 
-# Set a default URL
-baseurl="https://api.crowdstrike.com"
+	# Client ID, Client Secret for the API token. Then make a base64 version.
+	clientid=""
+	secret=""
+	b64creds=$( printf "$clientid:$secret" | /usr/bin/iconv -t ISO-8859-1 | /usr/bin/base64 -i - )
 
-# Now use this to work out the API base url
-# Highly modified from @Dj Padzensky 's work which I was linked to PR
-# on mac admin's slack
-baseurl=$( /usr/bin/curl -s -v -X POST -d "client_id=${clientid}&client_secret=${secret}" "${baseurl}/oauth2/token" 2>&1 | awk '($2 == "Location:") {print $3}' | cut -d/ -f1-3 )
-[ -z "$baseurl" ] && baseurl="https://api.crowdstrike.com"
+	# Set a default URL
+	baseurl="https://api.crowdstrike.com"
 
-# API Base URL and the various endpoints we need
-oauthtoken="$baseurl/oauth2/token"
-oauthrevoke="$baseurl/oauth2/revoke"
+	# Now use this to work out the API base url
+	# Highly modified from @Dj Padzensky 's work which I was linked to PR
+	# on mac admin's slack
+	baseurl=$( /usr/bin/curl -s -v -X POST -d "client_id=${clientid}&client_secret=${secret}" "${baseurl}/oauth2/token" 2>&1 | awk '($2 == "Location:") {print $3}' | cut -d/ -f1-3 )
+	[ -z "$baseurl" ] && baseurl="https://api.crowdstrike.com"
 
-# Define which version we want to get.
-# 0 means latest. 1 is N-1, 2 is N-2 and so on.
-version="1"
+	# API Base URL and the various endpoints we need
+	oauthtoken="$baseurl/oauth2/token"
+	oauthrevoke="$baseurl/oauth2/revoke"
 
-# Now define the API query we need
-sensorlist="$baseurl/sensors/combined/installers/v1?offset=${version}&limit=1&filter=platform%3A%22mac%22"
-sensordl="$baseurl/sensors/entities/download-installer/v1"
+	# Define which version we want to get.
+	# 0 means latest. 1 is N-1, 2 is N-2 and so on.
+	version="1"
 
-# Request bearer access token using the API
-token=$( /usr/bin/curl -s -X POST "$oauthtoken" -H "accept: application/json" -H "Content-Type: application/x-www-form-urlencoded" -d "client_id=${clientid}&client_secret=${secret}" )
+	# Now define the API query we need
+	sensorlist="$baseurl/sensors/combined/installers/v1?offset=${version}&limit=1&filter=platform%3A%22mac%22"
+	sensordl="$baseurl/sensors/entities/download-installer/v1"
 
-# Extract the bearer token from the json output above
-bearer=$( /usr/bin/plutil -extract access_token raw -o - - <<< "$token" )
+	# Request bearer access token using the API
+	token=$( /usr/bin/curl -s -X POST "$oauthtoken" -H "accept: application/json" -H "Content-Type: application/x-www-form-urlencoded" -d "client_id=${clientid}&client_secret=${secret}" )
 
-# Work out the CrowdStrike installer, grab the SHA256 hash and use that to download that installer
-sensorv=$( /usr/bin/curl -s -X GET "$sensorlist" -H "accept: application/json" -H "authorization: Bearer ${bearer}" )
-sensorname=$( /usr/bin/plutil -extract resources.0.name raw -o - - <<< "$sensorv" )
-sensorsha=$( /usr/bin/plutil -extract resources.0.sha256 raw -o - - <<< "$sensorv" )
+	# Extract the bearer token from the json output above
+	bearer=$( /usr/bin/plutil -extract access_token raw -o - - <<< "$token" )
 
-# Download the client. Retry if required up to 10 times.
-for loop in {1..10};
-do
-	echo "Download attempt: [$loop / 10]"
-	test=$( /usr/bin/curl -s -o /private/tmp/${sensorname} -H "Authorization: Bearer ${bearer}" -w "%{http_code}" "${sensordl}?id=${sensorsha}" )
-	[ "$test" = "200" ] && break
-done
+	# Work out the CrowdStrike installer, grab the SHA256 hash and use that to download that installer
+	sensorv=$( /usr/bin/curl -s -X GET "$sensorlist" -H "accept: application/json" -H "authorization: Bearer ${bearer}" )
+	sensorname=$( /usr/bin/plutil -extract resources.0.name raw -o - - <<< "$sensorv" )
+	sensorsha=$( /usr/bin/plutil -extract resources.0.sha256 raw -o - - <<< "$sensorv" )
 
-# Invalidate access to the bearer token
-/usr/bin/curl -X POST "$oauthrevoke" -H "accept: application/json" -H "authorization: Basic ${b64creds}" -H "Content-Type: application/x-www-form-urlencoded" -d "token=${bearer}"
+	# Download the client. Retry if required up to 10 times.
+	for loop in {1..10};
+	do
+		echo "Download attempt: [$loop / 10]"
+		test=$( /usr/bin/curl -s -o /private/tmp/${sensorname} -H "Authorization: Bearer ${bearer}" -w "%{http_code}" "${sensordl}?id=${sensorsha}" )
+		[ "$test" = "200" ] && break
+	done
 
-# Did the download actually work. Error if not.
-[ "$test" != "200" ] && { echo "Download failed. Exiting."; exit 1; }
+	# Invalidate access to the bearer token
+	/usr/bin/curl -X POST "$oauthrevoke" -H "accept: application/json" -H "authorization: Basic ${b64creds}" -H "Content-Type: application/x-www-form-urlencoded" -d "token=${bearer}"
 
-# Finally install and clean up
-/usr/sbin/installer -target / -pkg /private/tmp/${sensorname}
-/bin/rm /private/tmp/${sensorname}
+	# Did the download actually work. Error if not.
+	[ "$test" != "200" ] && { echo "Download failed. Exiting."; exit 1; }
+
+	# Finally install and clean up
+	/usr/sbin/installer -target / -pkg /private/tmp/${sensorname}
+	/bin/rm /private/tmp/${sensorname}
+else
+	echo "Crowdstrike already present."
+fi
 
 # All done!
 exit 0
