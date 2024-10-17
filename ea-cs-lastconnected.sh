b4fc176ee766a6ee9c8c259828a6e12e5415ad31
Richard Purves <franton@users.noreply.github.com>
2024-10-17 09:36:34 -0700

    Update ea-cs-lastconnected.sh
    With fixes inspired by @sdagley and @rayjd on Mac Admins Slack

--- ea-cs-lastconnected.sh
+++ ea-cs-lastconnected.sh
@@ -2,22 +2,22 @@
 
 # Report the last Crowdstrike connection to the cloud
 # Original EA: https://github.com/zoocoup/CrowdstikeEAsforJamfPro/blob/main/CrowdstrikeFalconv6LastEstablishedEA.sh
+# With fixes inspired by @sdagley and @rayjd on Mac Admins Slack
 
-falconctl=$( /usr/bin/find /Applications -iname "falconctl" -type f -maxdepth 4 )
+falconctl=$( /usr/bin/find /Applications/Falcon.app -iname "falconctl" -type f -maxdepth 3 )
 
 if [ "$falconctl" ];
 then
-    test=$( "$falconctl" stats Communications | /usr/bin/awk '/Cloud Activity | Last Established At/ {print $4,$5,$6,$8; exit;}' )
+    test=$( "$falconctl" stats Communications | /usr/bin/awk '/Cloud Activity | Established At/ {print $3,$4,$5,$7; exit;}' );
 
-    # Did we get output? If not try again with alternative check
-    [ ! -z "$test" ] && { test=$( "$falconctl" stats | /usr/bin/awk '/Cloud Activity | Established At/ {print $3,$4,$5,$7; exit;}' ); }   
-
+    [ ! -z "$test" ] && { test=$( "$falconctl" stats Communications | /usr/bin/awk '/Cloud Activity | Last Established At/ {print $4,$5,$6,$8; exit;}' ) }
+    
     if [ ! -z "$test" ];
     then
-	echo "<result>$( /bin/date -j -f "%b %d, %Y %H:%M:%S" "$test" "+%Y-%m-%d %H:%M:%S" )</result>"
+        echo "<result>$( /bin/date -j -f "%b %d, %Y %H:%M:%S" "$test" "+%Y-%m-%d %H:%M:%S" )</result>"
     else
     	echo "<result>1970-01-01 09:00:00</result>"
     fi
 else
-	echo "<result>1970-01-01 09:00:00</result>"
+    echo "<result>1970-01-01 09:00:00</result>"
 fi
